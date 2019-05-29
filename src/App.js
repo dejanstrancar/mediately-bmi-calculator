4e0409f8db4d3884e8e553eaec51b9d99e502bd5
Dejan Strancar <dejan@apto.si>
2019-05-29 14:32:12 +0200

    adding global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import meterTableRes from "./data/meterTable.json";
+import { createGlobalStyle } from "styled-components";
+import Styled from "./App.styles";
 
 import Header from "./components/Header";
 import CalculationForm from "./components/CalculationForm";
@@ -8,22 +10,33 @@ import IntroductionBlock from "./components/IntroductionBlock";
 import ResultBlock from "./components/ResultBlock";
 
 function App() {
+  const GlobalStyle = createGlobalStyle`
+  body,html {
+    margin:0;
+    padding:0;
+    font-family: Arial, sans-serif;
+  }
+`;
+
   return (
-    <div className="App">
-      <Header />
-      <ResultBlock
-        meter={{
-          color: "color orange",
-          label: "Severly underweight",
-          value: "16-16,99",
-          min: 16,
-          max: 16.99
-        }}
-      />
-      <CalculationForm />
-      <ReferenceValues meterTable={meterTableRes} />
-      <IntroductionBlock />
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <Styled.app>
+        <Header />
+        <ResultBlock
+          meter={{
+            color: "color orange",
+            label: "Severly underweight",
+            value: "16-16,99",
+            min: 16,
+            max: 16.99
+          }}
+        />
+        <CalculationForm />
+        <ReferenceValues meterTable={meterTableRes} />
+        <IntroductionBlock />
+      </Styled.app>
+    </React.Fragment>
   );
 }
 
---
+++ src/App.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default {
+  app: styled.div`
+    margin: 0;
+    min-height: calc(100vh - 10px - 10px);
+    width: calc(100% - 10px - 10px);
+    padding: 10px;
+    background: #e9ecef;
+  `
+};
