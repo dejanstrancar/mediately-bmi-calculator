b60e94bcbc19fcceee34c4682bdbea81dcff34ba
Dejan Strancar <dejan@apto.si>
2019-05-29 14:52:53 +0200

    styling reference values

--- src/components/ReferenceValues/ReferenceValues.jsx
+++ src/components/ReferenceValues/ReferenceValues.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import MeterRecord from "../_globals/MeterRecord";
 import Card from "../_globals/Card";
+import Styled from "./ReferenceValues.styles";
 
 export default function ReferenceValues(props) {
   return (
     <Card>
-      <h2>Reference values</h2>
+      <Styled.title>Reference values</Styled.title>
       {props.meterTable.map(meter => (
-        <MeterRecord meter={meter} />
+        <Styled.meterWrapper>
+          <MeterRecord meter={meter} />
+        </Styled.meterWrapper>
       ))}
     </Card>
   );
---
+++ src/components/ReferenceValues/ReferenceValues.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default {
+  title: styled.h2`
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    font-weight: normal;
+    margin: 0 0 10px 0;
+  `,
+  meterWrapper: styled.div`
+    margin: 10px 0;
+  `
+};
