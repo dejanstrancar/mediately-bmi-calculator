0761a5d8e855e1f6ecd00d4372abcb7b8a2e1673
Dejan Strancar <dejan@apto.si>
2019-05-29 14:32:39 +0200

    styling introduction block

--- src/components/IntroductionBlock/IntroductionBlock.jsx
+++ src/components/IntroductionBlock/IntroductionBlock.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Card from "../_globals/Card";
+import Styled from "./IntroductionBlock.styles";
 
 export default function IntroductionBlock() {
   return (
     <Card>
-      <h2>Introduction</h2>
-      <p>
+      <Styled.title>Introduction</Styled.title>
+      <Styled.text>
         The body mass index (BMI) or Quetelet index is a value derived from the
         mass (weight) and height of an individual. The BMI is defined as the
         body mass divided by the square of the body height, and is universally
-        expressed in units of kg/m3, resulting from mass in kilograms and height
-        in meters.
-      </p>
+        expressed in units of kg/m&sup3;, resulting from mass in kilograms and
+        height in meters.
+      </Styled.text>
     </Card>
   );
 }
---
+++ src/components/IntroductionBlock/IntroductionBlock.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default {
+  title: styled.h2`
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    font-weight: normal;
+  `,
+  text: styled.p`
+    margin: 10px 0 0 0;
+    padding: 0;
+    font-size: 13px;
+  `
+};
