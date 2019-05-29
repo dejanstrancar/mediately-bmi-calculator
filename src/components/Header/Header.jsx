d370722757f137c7acf6cb74181900d16678ee00
Dejan Strancar <dejan@apto.si>
2019-05-29 14:32:31 +0200

    removing not needed card from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Card from "../_globals/Card";
 
 export default function Header() {
   return (
-    <Card>
+    <div>
       <h1>BMI 1.0</h1>
-    </Card>
+    </div>
   );
 }
